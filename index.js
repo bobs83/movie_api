4e60c306805b526634a7723bc67e019945101160
Bobby Singh <b830103@gmail.com>
2023-10-06 21:17:48 +0200

    Step 2.  1/2/3
    1.  require express in your “index.js” file.
    
    2. create an Express GET route located at the endpoint “/movies” that returns a JSON object containing data about your top 10 movies
    
    3. Create another GET route located at the endpoint “/” that returns a default textual response of your choosing.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const app = express();
+
+let topMovies = [
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+  },
+  {
+    title: "Lord of the Rings",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    title: "Twilight",
+    author: "Stephanie Meyer",
+  },
+];
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+app.get("/", (req, res) => {
+  res.send("These are my favorite movies!");
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
