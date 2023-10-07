f4f8dd963716636428f48ba304018ec10632c7db
Bobby Singh <b830103@gmail.com>
2023-10-07 22:18:02 +0200

    Debugged and cleanup

--- index.js
+++ index.js
@@ -50,17 +50,14 @@ let topMovies = [
     author: "Jean-Pierre Jeunet",
   },
 ];
-
 // USE requests
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(express.static("public"));
 
 // setup the logger morgan using the write stream created above
-app.use(morgan("combined", { stream: accessLogStream }));
-app.use(morgan("public"));
-
-// error handling middleware function
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send("Something Broke!");
 });
 
 //  GET requests
@@ -70,14 +67,6 @@ app.get("/movies", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("These are my favorite movies!");
-});
-
-app.get("/documentation.html", (req, res) => {
-  res.sendFile("public", { root: __dirname });
-});
-
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret url with super top-secret content.");
 });
 
 //listen for requests
