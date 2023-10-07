b135c641b75d34e69b3140aba12c3f65563d42bc
Bobby Singh <b830103@gmail.com>
2023-10-07 21:06:10 +0200

    Completed Step2 4/5/6
    Use express.static to serve your “documentation.html” file
    
    Use the Morgan middleware library to log all request
    
    Create an error-handling middleware function

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const express = require("express");
+const express = require("express"),
+  fs = require("fs"),
+  morgan = require("morgan"),
+  path = require("path");
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
 
-let topMovies = [
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: "J.K. Rowling",
-  },
-  {
-    title: "Lord of the Rings",
-    author: "J.R.R. Tolkien",
-  },
-  {
-    title: "Twilight",
-    author: "Stephanie Meyer",
-  },
-];
+let topMovies = [];
 
+// USE requests
+
+// setup the logger morgan using the write stream created above
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan("public"));
+
+// error handling middleware function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+//  GET requests
 app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
@@ -24,6 +31,15 @@ app.get("/", (req, res) => {
   res.send("These are my favorite movies!");
 });
 
+app.get("/documentation.html", (req, res) => {
+  res.sendFile("public", { root: __dirname });
+});
+
+app.get("/secreturl", (req, res) => {
+  res.send("This is a secret url with super top-secret content.");
+});
+
+//listen for requests
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
