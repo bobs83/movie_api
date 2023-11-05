080f5bb1e553787675192abf83d3265db2fa020a
Bobby Singh <b830103@gmail.com>
2023-11-05 21:37:35 +0100

    debugged passport

--- passport.js
+++ passport.js
@@ -14,23 +14,30 @@ passport.use(
       passwordField: "Password",
     },
     async (username, password, callback) => {
-      await Users.findOne({ Username: username })
-        .then((user) => {
-          if (!user) {
-            console.log("incorrect username");
-            return callback(null, false, {
-              message: "Incorrect username or password.",
-            });
-          }
-          console.log("You are logged in!");
-          return callback(null, user);
-        })
-        .catch((error) => {
-          if (error) {
-            console.log(error);
-            return callback(error);
-          }
-        });
+      console.log(`${username} ${password}`);
+      try {
+        const user = await Users.findOne({ Username: username }); // Make sure the field is 'Username' if it's case-sensitive in your schema
+
+        if (!user) {
+          console.log("Incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        console.log("You are logged in!");
+
+        if (!user.validatePassword(password)) {
+          console.log("Incorrect password");
+          return callback(null, false, { message: "Incorrect password" });
+        }
+
+        console.log("Finished");
+        return callback(null, user);
+      } catch (err) {
+        console.error(err);
+        return callback(err);
+      }
     }
   )
 );
@@ -42,13 +49,33 @@ passport.use(
       secretOrKey: "your_jwt_secret",
     },
     async (jwtPayload, callback) => {
-      return await Users.findById(jwtPayload._id)
-        .then((user) => {
-          return callback(null, user);
-        })
-        .catch((error) => {
-          return callback(error);
-        });
+      // Log the jwtPayload for debugging
+      console.log(jwtPayload);
+
+      // Check if the payload contains the id property
+      if (!jwtPayload || !jwtPayload.id) {
+        return callback(new Error("Invalid token: ID is missing."), false);
+      }
+
+      try {
+        const user = await Users.findById(jwtPayload.id); // Using id instead of _id
+
+        // If no user is found, return the callback with null user
+        if (!user) {
+          return callback(null, false); // 'false' indicates that the authentication failed
+        }
+
+        // If a user is found, prepare the user object for the callback
+        const loggingUser = {
+          id: user._id, // This is the id used in the JWTStrategy
+        };
+
+        // Return the callback with the user object
+        return callback(null, loggingUser);
+      } catch (error) {
+        // In case of an error, return the callback with the error
+        return callback(error);
+      }
     }
   )
 );
